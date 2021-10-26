aafc417ebe84b716fd8bd6193ff171a04478d14d
VeraDidenko <verochka1998@gmail.com>
2021-10-26 19:25:50 +0300

    typescript example

---
+++ vue3/typescript/src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './App.css'
+
+import "flexmonster/flexmonster.css"
+
+createApp(App).use(router).mount('#app')
